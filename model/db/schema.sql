d8954eaf7ae163fbce6573ca029b6538ece44d46
Ben Calvert <benhcalvert@gmail.com>
2020-02-03 18:55:02 -0700

    seeds.sql

--- model/db/schema.sql
+++ model/db/schema.sql
@@ -3,3 +3,12 @@ DROP DATABASE IF EXISTS burgerdb;
 CREATE DATABASE budgerdb;
 
 USE burgerdb;
+
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers (
+id int NOT NULL AUTO_INCREMENT,
+burger_name varchar(255) NOT NULL,
+devoured BOOL DEFAULT false,
+PRIMARY KEY (id)
+);
---
+++ model/db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('BisonBurger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('ElkBurger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('CheeseBurger', FALSE);
